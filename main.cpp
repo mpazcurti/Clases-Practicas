98d58e1e1f21fc126f0ef490e980899d9b236abd
JuanMigliorisi <38170029+JuanMigliorisi@users.noreply.github.com>
2022-08-26 21:23:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-
 int main()
-{
-    DigitalIn gasDetector(D2);
+{   
+    DigitalIn gasDetector(D2); //instancio el objeto "gasDetector" de la Clase publica "DigitalIn" con la SOBRECARGA del CONSTRUCTOR que toma un parametro
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1); //instancio el objeto "alarmLed" de la Clase publica "DigitalOut" con la SOBRECARGA del CONSTRUCTOR que toma un parametro
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown);// Se establece el modo de pin a traves del metodo mode de la clase DigitalIn
 
-    alarmLed = OFF;
-
+    alarmLed = OFF; //Se inicializa la variable alarmLed
+printf("Hola Mundo\n"); // Se imprime el mensaje "Hola Mundo" por consola.
     while (true) {
         if ( gasDetector == ON ) {
             alarmLed = ON;
